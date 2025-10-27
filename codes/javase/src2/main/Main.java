3f1a08a997ccef8e223a14b9bdec87e4b81bdb3e
Baldwinnn <qasimov.aslan.2011@gmail.com>
2025-10-27 19:22:37 +0400

    Create Main.java
    commit

---
+++ codes/javase/src2/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Adım: Aslan");
+        System.out.println("Soyadım: Qasımov");
+        System.out.println("Yaşım: 15");
+    }
+}
