08f585e27e538465fbbc0fa8525e9ff51196507a
Baldwinnn <qasimov.aslan.2011@gmail.com>
2025-10-27 20:55:58 +0400

    commit

--- codes/javase/src2/main/Main.java
+++ codes/javase/src2/main/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Adım: Aslan");
         System.out.println("Soyadım: Qasımov");
-        System.out.println("Yaşım: 15");
+        System.out.println("Yaşım: 15 1");
     }
 }
