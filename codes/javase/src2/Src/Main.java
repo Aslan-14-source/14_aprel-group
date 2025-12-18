88c208acee4bd0e089380a46e09112d94514e929
Baldwinnn <qasimov.aslan.2011@gmail.com>
2025-12-18 18:57:54 +0400

    Summary

--- codes/javase/src2/Src/Main.java
+++ codes/javase/src2/Src/Main.java
@@ -1,34 +1,34 @@
 package Src;
 
 public class Main {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        String name = "Məhərrəm";
-        System.out.println(name);
+		String name = "Məhərrəm";
+		System.out.println(name);
 
-        String message = "Java dilini öyrənmək maraqlı və faydalıdır";
-        System.out.println(message);
+		String message = "Java dilini öyrənmək maraqlı və faydalıdır";
+		System.out.println(message);
 
-        System.out.println(message.length());
-        System.out.println(message.charAt(6));
-        System.out.println(message.indexOf('n'));
-        System.out.println(message.toLowerCase());
-        System.out.println(message.toUpperCase());
-        System.out.println(message.contains("Java"));
-        System.out.println(message.endsWith("dır"));
-        System.out.println(message.startsWith("Java"));
+		System.out.println(message.length());
+		System.out.println(message.charAt(6));
+		System.out.println(message.indexOf('n'));
+		System.out.println(message.toLowerCase());
+		System.out.println(message.toUpperCase());
+		System.out.println(message.contains("Java"));
+		System.out.println(message.endsWith("dır"));
+		System.out.println(message.startsWith("Java"));
 
-        name = " Ayxan ";
-        System.out.println(name.trim());
+		name = " Ayxan ";
+		System.out.println(name.trim());
 
-        name = "Kamil";
-        System.out.println(name.substring(0, 4));
+		name = "Kamil";
+		System.out.println(name.substring(0, 4));
 
-        name = "Hüseyn Mehdizade";
-        String[] parts = name.split(" ");
+		name = "Hüseyn Mehdizade";
+		String[] parts = name.split(" ");
 
-        for (String p : parts) {
-            System.out.println(p);
-        }
-    }
+		for (String p : parts) {
+			System.out.println(p);
+		}
+	}
 }
