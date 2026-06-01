636f7bfda21154baf715b88a5374330abf2eca61
Baldwinnn <qasimov.aslan.2011@gmail.com>
2026-06-01 20:46:32 +0400

    Summary

---
+++ codes/spring/ComputerShopping/src/main/java/az/developia/ComputerShopping/ComputerShoppingApplication.java
@@ -0,0 +1,13 @@
+package az.developia.ComputerShopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerShoppingApplication.class, args);
+	}
+
+}
